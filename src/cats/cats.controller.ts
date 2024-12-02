311289cb686183cb1c155bc6ca690912dc009d10
Herman Ho <hermanho@users.noreply.github.com>
2024-12-02 18:58:30 +0000

    Init

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Param, Post, RawBody, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Roles } from '../common/decorators/roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './interfaces/cat.interface';
+
+@UseGuards(RolesGuard)
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) { }
+
+  @Post()
+  @Roles(['admin'])
+  async create(
+    @Body(new ValidationPipe()) createCatDto: CreateCatDto,
+    @RawBody(new ValidationPipe()) rawBody: Buffer
+  ) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id', new ParseIntPipe())
+    id: number,
+  ) {
+    // get by ID logic
+  }
+}
